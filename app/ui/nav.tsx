b6b9983672ee6f2ee0bd7c7d705130ba076bc0e5
Roddy <124875320+RODDY064@users.noreply.github.com>
2024-03-06 22:25:52 +0000

    figma design added

---
+++ app/ui/nav.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Links:string[] = [
+    "Console",
+    "Games",
+    "Community",
+    "Accessories"
+]
+
+
+
+export default function Nav() {
+  return (
+    <div className="w-full flex items-center justify-between p-6 ">
+      <div className="flex md:gap-4 items-center">
+        <div className="w-[2rem] h-[2rem] grid grid-cols-2 grid-rows-2 gap-[2px]">
+          {[1,2,3,4].map((item)=>(
+          <div key={item} className="w-full h-full rounded-sm bg-neutral-200"></div>
+          ))}
+        </div>
+        <div className="flex gap-4 items-center">
+          <Image src="/icons/logo.jpg" width={170} height={10} alt="logo"/>
+          <div className="items-center sm:gap-4 xl:gap-10 hidden sm:flex">
+            {Links.map((item)=>(
+             <Link key={item} href="/" className="font-bold text-black/70 hover:text-lime-600">{item}</Link>
+            ))
+            }
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-6 items-center">
+        <div className="w-[15rem]  h-10 bg-neutral-100/70 border border-lime-600 rounded-[25px] hidden xl:block">
+            <input placeholder="searching..." className="w-[90%] h-full bg-transparent text-black/70  focus:outline-none p-2"/>
+        </div>
+        <div>Hello</div>
+      </div>
+    </div>
+  );
+}
